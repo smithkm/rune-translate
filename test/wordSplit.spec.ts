3f14100fec023da800a7052af73953ae7e26225f
Kevin Smith <smithkm@draconic.ca>
2023-01-07 14:47:18 -0800

    Testing word splitting

--- test/wordSplit.spec.ts
+++ test/wordSplit.spec.ts
@@ -15,3 +15,34 @@ test('One word', ()=> {
 
     expect(result).toStrictEqual([{type:"word", value:"blah"}]);
 }) 
+
+test('Two words', ()=> {
+    let translator = RuneTranslator.sosarian();
+
+    let result:Token[] = translator.splitWords("two words");
+
+    expect(result).toStrictEqual([
+        {type: "word", value: "two"},
+        {type: "space", value: " "},
+        {type: "word", value: "words"}]);
+}) 
+
+test('Two words with punctuation', ()=> {
+    let translator = RuneTranslator.sosarian();
+
+    let result:Token[] = translator.splitWords("two. words");
+
+    expect(result).toStrictEqual([
+        {type: "word", value: "two"},
+        {type: "punctuation", value: ". "},
+        {type: "word", value: "words"}]);
+})
+
+test('Hyphenated word', ()=> {
+    let translator = RuneTranslator.sosarian();
+
+    let result:Token[] = translator.splitWords("hyphenated-word");
+
+    expect(result).toStrictEqual([
+        {type: "word", value: "hyphenated-word"}]);
+})
